b6fc24e67a7920eec2e520a7fb931055a6a2a01c
Stephen Tvedt <stephen@stephentvedt.com>
2020-05-01 12:06:26 -0400

    deleting unused boilerplate

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ProductListing from "./ProductListing";
-import "./App.css";
-
-function App() {
-  return <ProductListing />;
-}
-
-export default App;
--- src/ProductListing.jsx
+++ src/ProductListing.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 import products from "./data/products.json";
 import ingredients from "./data/ingredients.json";
 
